795e697f098530251ff823567cd58d0b33418859
mineco13 <mineco13@users.noreply.github.com>
2026-02-22 11:26:14 +0900

    fix: broken path

--- gen-dirname.sh
+++ gen-dirname.sh
@@ -3,5 +3,5 @@ dir=$1
 remote=$(git remote get-url origin | sed 's/\.git$//')
 branch=$(git branch --show-current)
 url="$remote/tree/$branch/$dir"
-encoded=$(printf '%s' "$url" | base64 | tr '+/' '-_' | tr -d '=')
+encoded=$(printf '%s' "$url" | base64 | tr -d '\n' | tr '+/' '-_' | tr -d '=')
 printf '%s %s' "$dir" "$encoded"
